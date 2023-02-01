238e232a16d8681026feda879dc33a2dbd680d4d
Navaneeth Rajagopalan <navaneeth.rajagopalan.05@gmail.com>
2023-02-01 15:45:58 +1100

    Update with pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("Snowflake 102")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
